6495bac1047d126dd2711270172120b036886f2a
shiva mishra <127422257+shivamishra02@users.noreply.github.com>
2025-09-28 12:08:41 +0530

    Initial commit

---
+++ swiggy-top3-recco/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this product will help to reccomend the top 3 food items on swiggy',
+    author='shiva mishra',
+    license='',
+)
---
+++ swiggy-top3-recco/src/data/make_dataset.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+ #reading data from csv file
+def read_csv(file_path):
+    return pd.read_csv(file_path)
